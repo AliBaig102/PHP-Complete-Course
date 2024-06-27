907d5b4dafc37336bf95fb900e3c26dc5df86b13
AliBaig102 <baiga8424@gmail.com>
2024-06-28 02:41:10 +0500

    Introduction to PHP Overview of PHP Setting up the development environment (XAMPP/WAMP/MAMP) Writing your first PHP script: "Hello, World!"

---
+++ class01.php
@@ -0,0 +1,39 @@
+<?php
+//## Class 1: Introduction to PHP
+//- Overview of PHP
+//- Setting up the development environment (XAMPP/WAMP/MAMP)
+//- Writing your first PHP script: "Hello, World!"
+
+//What is PHP?
+
+//PHP is a server-side scripting language. It is used to develop dynamic web pages. PHP is an open source scripting language and free. PHP is easy to learn.
+
+//Setting up the development environment (XAMPP/WAMP/MAMP)
+
+//XAMPP is an open source PHP development environment. It is used to develop PHP applications.
+
+// Writing our first PHP script: "Hello, World!"
+
+//Hello, World!
+echo "Hello, World!";
+
+// What is echo?
+
+//echo is used to display output.
+
+// print vs echo
+
+//print is used to display output. echo is used to display output.
+
+//print "Hello, World!";
+//echo "Hello, World!";
+
+// Difference between print and echo
+// print just displays single value
+// echo displays multiple values
+
+// Example
+//$a=10;
+//print "Hello, World!",$a;
+//echo "Hello, World!",$a;
+?>
