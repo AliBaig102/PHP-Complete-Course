f0ef9cf2d2f73492c3d90fb11126b361335abd98
AliBaig102 <baiga8424@gmail.com>
2024-07-31 23:59:50 +0500

    validate register form and register fields

--- auth/actions/register.php
+++ auth/actions/register.php
@@ -1,6 +1,7 @@
 <?php
 require '../utils/functions.php';
 myRequire("utils/errors.php");
+myRequire("utils/auth.php");
 
 print_pre($_POST);
 $username = secure_input($_POST['username']);
@@ -9,34 +10,27 @@ $password = secure_input($_POST['password']);
 $confirm_password = secure_input($_POST['confirm_password']);
 
 if (empty_input($username)) {
-    error_and_redirect("signup_error", [
-        "username" => $username,
-        "username_error" => "Username is required"
-    ], "index.php");
+    error_and_redirect("signup", ["username_error" => "Username is required"], "index.php");
+} elseif (empty_input($email)) {
+    error_and_redirect("signup", ["username" => $username, "email_error" => "Email is required"], "index.php");
+}elseif (!validate_email($email)) {
+    error_and_redirect("signup", ["username" => $username, "email_error" => "Invalid email"], "index.php");
 }
-if (empty_input($email)) {
-    error_and_redirect("signup_error", ["email"=>$email, "email_error" => "Email is required"], "index.php");
-}
-if (empty_input($password)) {
-    error_and_redirect("signup_error", ["password" => "Password is required"], "index.php");
-}
-if (empty_input($confirm_password)) {
-    error_and_redirect("signup_error", ["confirm_password" => "Confirm password is required"], "index.php");
-}
-if ($password !== $confirm_password) {
-    error_and_redirect("signup_error", ["password" => "Passwords don't match"], "index.php");
+elseif (empty_input($password)) {
+    error_and_redirect("signup", ["username" => $username, "email" => $email, "password_error" => "Password is required"], "index.php");
+} elseif (empty_input($confirm_password)) {
+    error_and_redirect("signup", ["username" => $username, "email" => $email, "confirm_password_error" => "Confirm password is required"], "index.php");
+} elseif ($password !== $confirm_password) {
+    error_and_redirect("signup", ["username" => $username, "email" => $email, "password_error" => "Passwords don't match"], "index.php");
+}else{
+//    hash the password before storing in the database
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    if (register_user($username, $email, $password)) {
+        redirect("index.php");
+    } else {
+        error_and_redirect("signup", ["username" => $username, "email" => $email, "password_error" => "Something went wrong"], "index.php");
+    }
 }
 
-activeLink("index.php");
-//} elseif (empty_input($email)){
-//    error_and_redirect("Email is empty", "index.php");
-//} elseif (empty_input($password)){
-//    error_and_redirect("Password is empty", "index.php");
-//} elseif (empty_input($confirmPassword)){
-//    error_and_redirect("Confirm password is empty", "index.php");
-//} elseif ($password !== $confirmPassword){
-//    error("Passwords don't match");
-//} else {
-////    register($username, $email, $password);
-//}
 
