da1eba907ad8aa43741f83211432cbd885ed0d3d
AliBaig102 <baiga8424@gmail.com>
2024-07-31 23:55:34 +0500

    improved error using sessions

--- auth/partials/signup-popup.php
+++ auth/partials/signup-popup.php
@@ -1,26 +1,34 @@
 <?php
 $activeClass = "";
-    if (isset($_GET['signup_error'])) {
-        $username_error = $_GET['username_error'] ?? "";
-        $email_error = $_GET['email_error'] ?? "";
-        $password_error = $_GET['password'] ?? "";
-        $confirm_password_error = $_GET['confirm_password'] ?? "";
-        $username= $_GET['username'] ?? "";
-        $email = $_GET['email'] ?? "";
-        $activeClass = "active";
-    }
+$username_error = "";
+$email_error = "";
+$password_error = "";
+$confirm_password_error = "";
+$username = "";
+$email = "";
+if (isset($_SESSION['errors']["signup"])) {
+//    print_pre($_SESSION["errors"]['signup']);
+    $activeClass = "active";
+    $username_error = $_SESSION["errors"]['signup']['username_error'] ?? "";
+    $email_error = $_SESSION["errors"]['signup']['email_error'] ?? "";
+    $password_error = $_SESSION["errors"]['signup']['password_error'] ?? "";
+    $confirm_password_error = $_SESSION["errors"]['signup']['confirm_password_error'] ?? "";
+    $username = $_SESSION["errors"]['signup']['username'] ?? "";
+    $email = $_SESSION["errors"]['signup']['email'] ?? "";
+    unset($_SESSION["errors"]['signup']);
+}
 ?>
 <div class="popup-container signup-popup <?php echo $activeClass; ?>">
     <form action="actions/register.php" method="post">
         <h3>Register</h3>
         <div class="input-group">
             <label for="username"><i class="fas fa-user"></i></label>
-            <input type="text" name="username" id="username" value="<?= $username ?? '' ?>" placeholder="Username">
+            <input type="text" name="username" id="username" value="<?= $username ?>" placeholder="Username">
         </div>
             <span class="error"><?= $username_error; ?></span>
         <div class="input-group">
             <label for="email"><i class="fas fa-envelope"></i></label>
-            <input type="email" name="email" id="email" value="<?= $email ?? '' ?>" placeholder="Email">
+            <input type="email" name="email" id="email" value="<?= $email ?>" placeholder="Email">
         </div>
         <span class="error"><?= $email_error; ?></span>
         <div class="input-group">
