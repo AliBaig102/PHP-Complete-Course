d08caa0253b0bfab0408a87d32c7e383b259e5ca
AliBaig102 <baiga8424@gmail.com>
2024-08-01 09:59:18 +0500

    change register function

--- auth/utils/auth.php
+++ auth/utils/auth.php
@@ -1,13 +1,13 @@
 <?php
-require "functions.php";
+require_once "functions.php";
 myRequire("database/connection.php");
 
-function register_user($username, $email, $password): bool
+function register_user($username, $email, $password, $verification_code, $code_expiration): bool
 {
     global $connection;
-    $sql = "INSERT INTO auth (name,email,password) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO auth (name,email,password,verification_code,code_expiration) VALUES (?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($connection, $sql);
-    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $password);
+    mysqli_stmt_bind_param($stmt, "ssss", $username, $email, $password, $verification_code, $code_expiration);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
     if (mysqli_stmt_affected_rows($stmt) > 0) {
