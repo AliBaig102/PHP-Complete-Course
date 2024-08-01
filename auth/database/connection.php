d371e976a0691b4a44d5001f9ac41541d655b759
AliBaig102 <baiga8424@gmail.com>
2024-08-01 10:00:48 +0500

    improved connection.php

--- auth/database/connection.php
+++ auth/database/connection.php
@@ -1,21 +1,8 @@
 <?php
-require dirname(__DIR__) . "/vendor/autoload.php";
-use  Dotenv\Dotenv;
+require_once dirname(__DIR__) . '/constant/constant.php';
 
-try {
-    $dotenv = Dotenv::createImmutable(__DIR__."/.."); // Adjust path if necessary
-    $dotenv->load();
-    $host = $_ENV['DB_HOST'];
-    $user = $_ENV['DB_USER'];
-    $pass = $_ENV['DB_PASS'];
-    $db = $_ENV['DB_NAME'];
-
-    $connection = mysqli_connect($host, $user, $pass, $db);
-
-    if (!$connection) {
-        die("Error connecting to database: " . mysqli_connect_error());
-    }
-} catch (\Dotenv\Exception\InvalidPathException $e) {
-    // Handle .env file error
-    echo "Error loading .env file: " . $e->getMessage();
+global $db_host, $db_user, $db_pass, $db_name;
+$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$connection) {
+    die("Error connecting to database: " . mysqli_connect_error());
 }
