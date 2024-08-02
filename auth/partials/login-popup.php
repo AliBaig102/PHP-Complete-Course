195d973cda854cf45dc70d60588033c1487fc4c3
AliBaig102 <baiga8424@gmail.com>
2024-08-02 11:42:46 +0500

    working in login

--- auth/partials/login-popup.php
+++ auth/partials/login-popup.php
@@ -1,4 +1,35 @@
-<div class="popup-container login-popup ">
+<?php
+$activeClass = "";
+$verify_success_message = "";
+if (isset($_SESSION["success"]['verify'])) {
+    $activeClass = "active";
+    $verify_success_message = $_SESSION["success"]['verify']['message'] ?? "";
+    if (!empty($verify_success_message)) {
+        echo '<script>
+            Toastify({
+                text: "' . $verify_success_message . '",
+                duration: 3000,
+                close: true,
+                offset: {
+                    y: 30
+                },
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                    background: "#222",
+                    borderBottom: "3px solid #fd9329",
+                    padding: "10px 20px",
+                    boxShadow: "0 0 5px #444",
+                },
+                onClick: function(){} // Callback after click
+            }).showToast();
+        </script>';
+    }
+    unset($_SESSION["success"]['verify']);
+}
+?>
+<div class="popup-container login-popup  <?php echo $activeClass; ?>">
     <form action="">
         <h3>Login</h3>
         <div class="input-group">
