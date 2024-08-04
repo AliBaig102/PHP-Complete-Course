8979ebcc71bb09f5e71bbad0c8bfe6d4590cf38f
AliBaig102 <baiga8424@gmail.com>
2024-08-04 23:49:02 +0500

    condition-based buttons

--- auth/partials/nav.php
+++ auth/partials/nav.php
@@ -8,8 +8,19 @@
         <li><a class="<?= activeLink('contact.php') ?>" href="contact.php">Contact</a></li>
         <li><a class="<?= activeLink('dashboard.php') ?>" href="dashboard.php">Dashboard</a></li>
     </ul>
-    <ol>
-        <li class="login-btn">Login</li>
-        <li class="signup-btn">Register</li>
-    </ol>
+    <?php
+    if (is_user_logged_in()) {
+        echo '<ol>
+                <h4>Hello ' . $_SESSION['user']['name'] . '</h4>              
+                <li class="logout-btn"><a href="actions/logout.php">Logout</a></li>
+            </ol>';
+
+    } else {
+        echo '<ol>
+                <li class="login-btn">Login</li>
+                <li class="signup-btn">Register</li>
+            </ol>';
+    }
+    ?>
+
 </nav>
